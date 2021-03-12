043aa96718f1637ea3a1dfa406fc278a513c6687
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2021-03-12 17:34:34 -0300

    first commit

---
+++ src/main/java/br/com/sinart/mapSys/MapSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.sinart.mapSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapSysApplication.class, args);
+	}
+
+}
